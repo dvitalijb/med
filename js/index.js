a75ad04994dea76f170af74f73d4ff6e49b66f3d
Vitalii <42237546+dvitalijb@users.noreply.github.com>
2020-07-08 20:30:20 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,167 @@
-// const menuBurger = document.getElementById('menu');
-// const sidePanel = document.getElementById('side-panel');
-// const body = document.getElementById('body');
+$(document).ready(function () {
+  const $menu = $("#menu");
+  const $sidePanel = $("#side-panel");
 
-//let isOpenSideMenu = false;
+  const isValidMail = (field) =>
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field.val().trim());
+  const isValidName = (field) => /^[A-Za-z]+$/.test(field.val().trim());
+  const isValidTel = (field) => /^\d{10}$/.test(field.val().trim());
 
-// const openSidePanel = () => {
-//     isOpenSideMenu = true;
-//     sidePanel.style.width = "255px"
-// };
+  $(document).mouseup((e) => {
+    if (
+      !$sidePanel.is(e.target) && // if the target of the click isn't the container...
+      $sidePanel.has(e.target).length === 0
+    ) {
+      // ... nor a descendant of the container
+      $sidePanel.removeClass("active");
+    }
+  });
 
-// menuBurger.addEventListener('click', () => openSidePanel());
-// sidePanel.addEventListener('click', () => {
-//     //console.log('sidePanel');
-// })
+  $menu.on("click", () => {
+    $sidePanel.addClass("active");
+  });
 
+  const form = document.getElementById("form");
+  const formModal = document.getElementById("form-modal");
+  let email;
+  let name;
+  let tel;
 
-const $menu = $('#menu');
-const $sidePanel = $('#side-panel');
+  const showError = () => formErrors.classList.add("active");
+  const addClassError = (field) => field[0].classList.add("error");
+  const addClassSuccess = (field) => field[0].classList.add("success");
+  const removeClassError = (field) => field.classList.remove("error");
+  const removeClassSuccess = (field) => field.classList.remove("success");
 
-$(document).mouseup(e => {
-   if (!$sidePanel.is(e.target) // if the target of the click isn't the container...
-   && $sidePanel.has(e.target).length === 0) // ... nor a descendant of the container
-   {
-       //console.log($menu.has(e.target).length);
-       $sidePanel.removeClass('active');
-  }
- });
+  // $(form).on("submit", function () {
+  //   event.preventDefault();
 
- $menu.on('click', () => {
-    $sidePanel.addClass('active');
+  //   for (input of form) {
+  //     removeClassError(input);
+  //     removeClassSuccess(input);
+  //   }
+
+  //   email = $("#email");
+  //   name = $("#name");
+  //   tel = $("#tel");
+
+  //   const isValidMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+  //     email.val().trim()
+  //   );
+  //   const isValidName = /^[A-Za-z]+$/.test(name.val().trim());
+  //   const isValidTel = /^\d{10}$/.test(tel.val().trim());
+
+  //   isValidMail ? addClassSuccess(email) : addClassError(email);
+  //   isValidName ? addClassSuccess(name) : addClassError(name);
+  //   isValidTel ? addClassSuccess(tel) : addClassError(tel);
+
+  //   if (isValidMail && isValidName && isValidTel) {
+  //     let formData = $(form).serialize();
+  //     $.ajax({
+  //       url: "mailer.php",
+  //       data: formData,
+  //       type: "POST",
+  //       success: function (data) {
+  //         alert(data);
+  //       },
+  //       error: function (error) {
+  //         alert(error);
+  //       },
+  //     });
+
+  //     form.reset();
+  //     for (input of form) {
+  //       removeClassError(input);
+  //       removeClassSuccess(input);
+  //     }
+  //   }
+  //   return false;
+  // });
+
+  $(form).on("submit", function () {
+    event.preventDefault();
+
+    for (input of form) {
+      removeClassError(input);
+      removeClassSuccess(input);
+    }
+
+    name = $("#name");
+    email = $("#email");
+    tel = $("#tel");
+
+    const checkName = isValidName(name);
+    const checkMail = isValidMail(email);
+    const checkTel = isValidTel(tel);
+
+    checkName ? addClassSuccess(name) : addClassError(name);
+    checkMail ? addClassSuccess(email) : addClassError(email);
+    checkTel ? addClassSuccess(tel) : addClassError(tel);
+
+    if (checkName && checkMail && checkTel) {
+      let formData = $(form).serialize();
+      $.ajax({
+        url: "mailer.php",
+        data: formData,
+        type: "POST",
+        success: function (data) {
+          alert(data);
+        },
+        error: function (error) {
+          alert(error);
+        },
+      });
+
+      form.reset();
+      for (input of form) {
+        removeClassError(input);
+        removeClassSuccess(input);
+      }
+    }
+    return false;
+  });
+
+  $(formModal).on("submit", function () {
+    event.preventDefault();
+
+    for (input of formModal) {
+      removeClassError(input);
+      removeClassSuccess(input);
+    }
+
+    name = $("#name-modal");
+    email = $("#email-modal");
+    tel = $("#tel-modal");
+
+    const checkName = isValidName(name);
+    const checkMail = isValidMail(email);
+    const checkTel = isValidTel(tel);
+
+    checkName ? addClassSuccess(name) : addClassError(name);
+    checkMail ? addClassSuccess(email) : addClassError(email);
+    checkTel ? addClassSuccess(tel) : addClassError(tel);
+
+    if (checkName && checkMail && checkTel) {
+      let formData = $(formModal).serialize();
+      $.ajax({
+        url: "mailer.php",
+        data: formData,
+        type: "POST",
+        success: function (data) {
+          alert(data);
+        },
+        error: function (error) {
+          alert(error);
+        },
+      });
+
+      formModal.reset();
+      for (input of formModal) {
+        removeClassError(input);
+        removeClassSuccess(input);
+      }
+      $(".modal-close")[0].click();
+    }
+    return false;
+  });
 });
-
-
-
